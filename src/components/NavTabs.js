c1abd8b22b7b57c217188aa911babe78f4406f03
Chuck Marlow <marlowchu@gmail.com>
2022-03-26 10:29:12 -0400

    deploying

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
@@ -16,7 +15,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          //  TODO: Add a comment explaining what this logic is doing
+         
 
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
@@ -29,7 +28,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#projects"
           onClick={() => handlePageChange('Projects')}
-          //  TODO: Add a comment explaining what this logic is doing
+        
 
           className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
         >
@@ -41,7 +40,7 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
+        
 
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
@@ -55,7 +54,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a class = "nav-text"
           href="#resume"
           onClick={() => handlePageChange('Resume')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+        
 
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
